d8af631b9325c42044977a210452af65652dc2bc
ohmin <71929898+dhalsdl12@users.noreply.github.com>
2023-01-19 14:13:34 +0900

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,23 @@
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from datetime import date, timedelta
+from github_setting import get_github_repo, upload_github_issue
 
 
+def extract_commit_data():
+    upload_contents = ''
+
+    for i in range(len(team)):
+        url = '\"' + url + team[i] + '\"'
+        name = team[i]
+        commit = commits[i]
+        content = f"<a href={url}>" + name + "</a>" + "<br/>"
+        content += "<blockquote data-ke-style=\"style2\">" + commit + "</blockquote><br/>\n"
+        upload_contents += content
+
+    return upload_contents
+    
+    
 def pageCrawl():
     html = drive.page_source
     soup = BeautifulSoup(html, 'html.parser')
@@ -13,18 +28,32 @@ def pageCrawl():
         d = a.attrs['data-date']
         if d == yesterday:
             commit = a.text.split()[0]
-            print(d, ":", commit)
+            commits.append(str(d) + ' : ' + str(commit))
 
 
-now = date.today()
-yesterday = (date.today() - timedelta(1)).strftime('%Y-%m-%d')
-team = ['dhalsdl12', 'seokiis', 'shgusgh12', 'wns0865']
-url = 'https://github.com/'
-driver = 'C:\\Users\\dhals\\Downloads\\chromedriver.exe'
+if __name__ == "__main__":
+    access_token = os.environ['MY_GITHUB_TOKEN']
+    repository_name = "KKNL_study"
+    
+    now = date.today()
+    yesterday = (date.today() - timedelta(1)).strftime('%Y-%m-%d')
+    team = ['dhalsdl12', 'seokiis', 'shgusgh12', 'wns0865']
+    url = 'https://github.com/'
+    driver = 'C:\\Users\\dhals\\Downloads\\chromedriver.exe'
 
-drive = webdriver.Chrome(driver)
-for i in range(4):
-    drive.get(url+team[i])
-    print(team[i])
-    pageCrawl()
-drive.close()
+    drive = webdriver.Chrome(driver)
+    for i in range(4):
+        drive.get(url+team[i])
+        pageCrawl()
+
+    for c in commits:
+        print(c)
+
+    drive.close()
+    
+    
+    issue_title = f"({yesterday}) github commit 했어?"
+    upload_contents = extract_commit_data()
+    repo = get_github_repo(access_token, repository_name)
+    upload_github_issue(repo, issue_title, upload_contents)
+    print("Upload Github Issue Success!")
