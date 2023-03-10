71ceb356e7b1f73301d7f369ac0a4875e85cdfe2
dhalsdl12 <dhalsdl12@gmail.com>
2023-03-10 12:49:26 +0900

    aa

--- test.py
+++ test.py
@@ -5,7 +5,6 @@ import time, os
 from selenium import webdriver
 from bs4 import BeautifulSoup
 from datetime import date, timedelta
-from github_setting import get_github_repo, upload_github_issue
 
 
 def extract_commit_data():
@@ -15,8 +14,7 @@ def extract_commit_data():
         link = '\"' + url + member[i] + '\"'
         name = names[i]
         st = streak[i]
-        tier = tiers[i]
-        content = f"<a href={link}>" + name + "    " + tier + "</a>" + "<br/>"
+        content = f"<a href={link}>" + name + "</a>" + "<br/>"
         content += "<blockquote data-ke-style=\"style2\">" + st + "</blockquote><br/>"
         upload_contents += content
 
@@ -24,23 +22,14 @@ def extract_commit_data():
 
 
 if __name__ == "__main__":
-    access_token = os.environ['MY_GITHUB_TOKEN']
     repository_name = "ABLT_MEMBER"
     now = date.today()
     yesterday = (date.today() - timedelta(1)).strftime('%Y-%m-%d')
     
-    '''
     options = webdriver.ChromeOptions()
     options.add_experimental_option("excludeSwitches", ["enable-logging"])
     driver = webdriver.Chrome(options=options)
-    '''
-    chrome_driver = os.path.join('chromedriver')
-    chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--headless')
-    chrome_options.add_argument('--no-sandbox')
-    chrome_options.add_argument('--disable-dev-shm-usage')
-    chrome_options.add_argument('--disable-gpu')
-    driver = webdriver.Chrome(chrome_driver, options=chrome_options)
+
     # driver 실행
 
     url = 'https://solved.ac/'
@@ -48,7 +37,7 @@ if __name__ == "__main__":
     member = ['dhalsdl12','shgusgh12', 'shinsion', 'wns0865']
     names = ['권오민', '노현호', '신시온', '이준형']
     streak = ['현재 0일', '현재 0일', '현재 0일', '현재 0일', '현재 0일']
-    tiers = []
+
     for i in range(len(member)):
         driver.get(url + member[i])
         time.sleep(1)
@@ -65,18 +54,9 @@ if __name__ == "__main__":
         solved = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div[4]/div/div[4]/div[1]/div[2]/div/div/div').text
         day = int(driver.find_element(By.XPATH, '//*[@id="__next"]/div/div[4]/div/div[4]/div[1]/div[2]/div/div/div/b').text)
         tier = driver.find_element(By.XPATH, '//*[@id="__next"]/div/div[3]/div[1]/div[4]/span[2]/b').text
-        tiers.append(tier)
-        print(names[i], solved)
-        if day == 0:
-            streak[i] = streak[i] + '  벌금입니다.'
-        else:
-            streak[i] = solved
+
+        print(tier)
 
 
     driver.close()
     
-    issue_title = f"어제({yesterday}) 알고리즘 문제 풀었어?"
-    upload_contents = extract_commit_data()
-    repo = get_github_repo(access_token, repository_name)
-    upload_github_issue(repo, issue_title, upload_contents)
-    print("Upload Github Issue Success!")
